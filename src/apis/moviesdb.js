18246d1caac256379e876381b666bcd4d1d0cc09
MsalgadoJ <mayra.rinconess@gmail.com>
2022-01-18 08:48:00 -0300

    updated env key

--- src/apis/moviesdb.js
+++ src/apis/moviesdb.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const KEY = '6f26fd536dd6192ec8a57e94141f8b20';
-
 export default axios.create({
   baseURL: 'https://api.themoviedb.org/3/movie/',
   params: {
-    api_key: KEY
+    api_key: process.env.REACT_APP_MOVIESDB_API_KEY
   }
 })
