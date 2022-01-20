166d4af4091be17368a23c7213d652a80ccef57b
MsalgadoJ <mayra.rinconess@gmail.com>
2022-01-20 20:52:58 -0300

    added firebase keys to env

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseApp = initializeApp({ 
-  apiKey: "AIzaSyDH-rmvYVH5kAvY4wI-Zzqf1qFFEZQvzYU",
-  authDomain: "liteflix-cd0b6.firebaseapp.com",
-  projectId: "liteflix-cd0b6",
-  storageBucket: "liteflix-cd0b6.appspot.com",
-  messagingSenderId: "992656210143",
-  appId: "1:992656210143:web:3682ccc2601ff86fbd516a",
-  measurementId: "G-KLNK52ZZZ8"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
  });
 
 const storage = getFirestore(firebaseApp)
